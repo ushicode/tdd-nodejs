dd94d46dd3bcf276f9cead91af5596a58c0c55a2
Collins U <collinsushi@gmail.com>
2022-11-19 23:06:58 +0000

    NoSQL Added

--- db/db.connect.js
+++ db/db.connect.js
@@ -3,14 +3,19 @@ require('dotenv').config()
 
 const connect = async ()=>{
    try {
-    await mongoose.connect(
+   const conn = await mongoose.connect(
         "mongodb+srv://"+process.env.DB_CREDENTIAL+".mongodb.net/?retryWrites=true&w=majority",
         {useNewUrlParser: true }
         );
-        console.log(`CONNECTION SUCCESS`);
+   const state = checkConnectState(mongoose.connection.readyState)
+       console.log(state);
    } catch (err) {
     console.log(`CONNECTION ERR : ${err.message}`);
    }
+}
+
+const checkConnectState = (value) => {
+   if(value === 1)return 'Connected to DB'; else return 'Disconnected from DB'
 }
 
 module.exports = {connect}
